3a09cf51802e906995670ddbce613903ba6acfbf
soCesca <asafrances6@gmail.com>
2022-03-16 15:06:54 -0700

    first task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,14 +8,14 @@
 
 int main(void)
 {
-	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
-	int count, sz;
+	int i;
+	char p[] = "_putchar";
 
-	sz = sizeof(str) / sizeof(int);
-	for (count = 0, count < sz; count++)
+	for (i = 0; i < 8; i++)
 	{
-		_putchar(str[count]);
+		_putchar(p[i]);
 	}
+	
 	_putchar('\n');
 	return (0);
 }
