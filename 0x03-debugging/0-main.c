f01c595cec1717fa94634006494df2b0237bc0b6
soCesca <asafrances6@gmail.com>
2022-03-17 18:03:29 -0700

    files

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - tests function prints if integer is +ve or -ve
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+
+	a = 0;
+	positive_or_negative(a);
+
+	return (0);
+}
