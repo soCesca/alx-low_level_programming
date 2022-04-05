33bad10cf80d26be0900ecfbe5f6ebfb32855709
soCesca <asafrances6@gmail.com>
2022-04-05 06:46:55 -0700

    static library

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 gcc -c -Wall -Werror -Wextra -pedantic *.c
-ar -rc libmy.a *.o
-ranlib libmy.a
-gcc -c -Wall -Werror -Wextra -pedantic 1-memcpy.c -o 1-memcpy.o
-ar -rc libmy.a 1-memcpy.o
-ranlib libmy.a
+ar -rc liball.a *.o
+ranlib liball.a
